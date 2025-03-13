406d6cdfcaf2d0e525c7db8ec69a11f1fe9196ab
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 10:17:45 +0700

    menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul4_103022300062;
+
+class Program
+{
+
+    static void Main()
+    {
+        //SOAL 1
+        String input = Console.ReadLine();
+
+        Enum.TryParse(input, true, out KodeProduk.Produk produk);
+        {
+            String kodeProduk = KodeProduk.getKodeProduk(produk);
+            Console.WriteLine($"Kode Produk {produk}: {kodeProduk}"); 
+
+        }
+    }
+}
